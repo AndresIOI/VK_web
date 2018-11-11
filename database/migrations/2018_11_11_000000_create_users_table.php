119fb8ac7b8d7c45c749ee1e894a4dd607b8ce61
andres_ioi <andres_ioi@hotmail.com>
2018-11-10 22:45:44 -0600

    Migraciones

--- database/migrations/2018_11_11_000000_create_users_table.php
+++ database/migrations/2018_11_11_000000_create_users_table.php
@@ -16,10 +16,15 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('apellido_materno',191);
+            $table->string('apellido_paterno',191);
+            $table->string('avatar',200);
+            $table->string('email',191)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->unsignedInteger('rol_id');
+            $table->foreign('rol_id')->references('id_rol')->on('rols');
             $table->timestamps();
         });
     }
